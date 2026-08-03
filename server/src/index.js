d6a444f21918c3f3be32c11e745a0c0bb558bc21
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2026-08-03 20:57:25 +0000

    feat: setup PostgreSQL and Prisma

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,28 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
-
+// Middlewares
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Health Check
 app.get("/health", (req, res) => {
   return res.status(200).json({
     success: true,
-    message: "Health is good",
+    message: "Server is healthy",
+    timestamp: new Date().toISOString(),
   });
 });
 
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
