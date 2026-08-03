d877f218a8ee931f2d4903ca301af37f3b642d73
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2026-08-03 20:05:32 +0000

    Initial project setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center">
+      <Button>Button testing</Button>
+    </div>
+  );
+}
