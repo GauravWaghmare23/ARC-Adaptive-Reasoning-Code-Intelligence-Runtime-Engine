5a1ce33bfd54d4c0b432150fdb7cdcfd440e1729
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2026-08-11 16:04:39 +0530

    chore: configure production deployment settings

--- client/src/lib/authClient.ts
+++ client/src/lib/authClient.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react";
 
 
 export const authClient = createAuthClient({
-    baseURL:"http://localhost:4000"
+    baseURL:"https://arc-x0es.onrender.com/"
 })
