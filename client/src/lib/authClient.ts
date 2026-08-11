05354317269fbb63179060318053d3f2241c8359
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2026-08-11 16:13:47 +0530

    fix: configure device authorization client

--- client/src/lib/authClient.ts
+++ client/src/lib/authClient.ts
@@ -1,6 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import { deviceAuthorizationClient } from "better-auth/client/plugins";
 
 
 export const authClient = createAuthClient({
-    baseURL:"https://arc-x0es.onrender.com/"
+    baseURL:"https://arc-x0es.onrender.com/",
+    plugins: [
+        deviceAuthorizationClient(),
+    ],
 })
